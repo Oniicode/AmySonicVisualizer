b389c6c9906400954a9d2fdde8db3c524ad8f16b
unknown <fuckemail>
2026-09-24 00:15:09 +0200

    Feature Complete: Dark Mode support.

--- AmySonicVisualizer/Program.cs
+++ AmySonicVisualizer/Program.cs
@@ -10,6 +10,7 @@ namespace AmySonicVisualizer
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            Application.SetColorMode(SystemColorMode.System);
             ApplicationConfiguration.Initialize();
             Application.Run(new ViewerForm());
         }
